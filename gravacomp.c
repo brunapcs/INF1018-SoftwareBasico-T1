cd5edabe77f0a91f4ea9bcb39ccbb8472377e143
mcostaconrado <mcostaconrado@gmail.com>
2017-10-05 13:56:45 -0300

    adiçao do looping na mostracomp. aparente finalizacao do trabalho

--- gravacomp.c
+++ gravacomp.c
@@ -5,16 +5,16 @@
  
 /* [INÍCIO] FUNÇÕES STATIC ( AUXILIARES ) */
 
-static int int_pow(int base, int exp) 							// função para cálculo de potência com int
-{
+static int int_pow(int base, int exp){ 						// função para cálculo de potência com int
     int result = 1;
-    while (exp)
-    {
+    
+    while (exp){
         if (exp & 1)
             result *= base;
         exp /= 2;
         base *= base;
     }
+    
     return result;
 }
 
@@ -27,6 +27,7 @@ static char contaBytesUnsigned(unsigned int x){	// função auxiliar para cálcu
         else
             i = 0;
     }
+    
     return bytes;
 }
 
@@ -34,11 +35,12 @@ static char contaBytesSigned(int x){ // função auxiliar para cálculo do numer
     char bytes = 4;
     
     for(int i = 3; i > 0; i--){										 
-        if ( x >= -int_pow(2,8*i-1) && x <= (int_pow(2, 8*i-1) - 1)) //range: -2^(8*n-1) -> 2^(8*n-1)-1
+        if (x >= -int_pow(2,8*i-1) && x <= (int_pow(2, 8*i-1) - 1)) //range: -2^(8*n-1) -> 2^(8*n-1)-1
             bytes--;
         else
             i = 0;
     }
+    
     return bytes;
 }
 
@@ -47,7 +49,6 @@ static char contaBytesSigned(int x){ // função auxiliar para cálculo do numer
 int gravacomp (int nstructs, void* valores, char* descritor, FILE *arquivo){
     char* aux_descritor;
     char nstructs_c = nstructs;
-    
     
     fwrite(&nstructs_c, sizeof(char), 1, arquivo); 			// Primeiro byte do arquivo com nstructs.
     
@@ -71,16 +72,16 @@ int gravacomp (int nstructs, void* valores, char* descritor, FILE *arquivo){
                     bytes = contaBytesUnsigned(*aux);
                     cabecalho |= bytes;								// grava o valor de bytes no cabeçalho
                 }
-                
+                 
                 aux_descritor ++;
-                if (aux_descritor[0] == '\0')						// se esse for o último campo do descritor, o 7º bit é ligado
+                if (aux_descritor[0] == '\0')
                     cabecalho |= 0x80;								// c = 1000 0000.
                 
                 fwrite(&cabecalho, sizeof(char), 1, arquivo);
                 valores = valores + (bytes - 1);			// faz valores apontar para o ultimo byte gravavel (mais signific)
                 
                 for (int i = 0; i < bytes; i++){
-                    fwrite(valores, sizeof(char), 1 , arquivo);	// big endian
+                    fwrite(valores, sizeof(char), 1 , arquivo);
                     valores--;									//valores-- até apontar para uma pos antes do inicio
                 }
                 valores = valores + 4 + 1;						// valores agora aponta para o início do próximo dado.
@@ -100,7 +101,7 @@ int gravacomp (int nstructs, void* valores, char* descritor, FILE *arquivo){
                 
                 aux_descritor += 3;									//pula 3 posiçoes(prox campo da struct)
                 
-                if (aux_descritor[0] == '\0' ) 						// se esse for o último campo do descritor, o 7º bit é ligado
+                if (aux_descritor[0] == '\0' ) 					
                     cabecalho |= 0x80;								// c = 1000 0000.
                 
                 cabecalho |= bytes; 
